2f0bea84c8b9dc1cc9f7c6d63ae77532fafc339c
Andy Huang <cerberus@ayxh.dev>
2024-09-05 15:21:17 +0000

    Remove unused imports

--- src/test/java/com/corndel/chessington/model/GameTest.java
+++ src/test/java/com/corndel/chessington/model/GameTest.java
@@ -1,7 +1,3 @@
 package com.corndel.chessington.model;
 
-import static com.corndel.chessington.model.pieces.PieceAssert.*;
-import static org.assertj.core.api.Assertions.*;
-
-public class GameTest {
-}
+public class GameTest {}
